2ec45d2ac44cb1c19d8da6e5600be8aeb92e334f
PsiFunction <shalaginmx998@yandex.ru>
2024-04-24 08:29:27 +0300

    + draw cards

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ fn setup(
     let layout = TextureAtlasLayout::from_grid(Vec2::new(240.0, 400.0), 8, 10, None, None);
     let texture_atlas_layout = texture_atlas_layouts.add(layout);
     // Use only the subset of sprites in the sheet that make up the run animation
-    let animation_indices = AnimationIndices { first: 1, last: 6 };
+    let animation_indices = AnimationIndices { first: 0, last: 79 };
     commands.spawn(Camera2dBundle::default());
     commands.spawn((
         SpriteSheetBundle {
@@ -51,10 +51,9 @@ fn setup(
                 layout: texture_atlas_layout,
                 index: animation_indices.first,
             },
-            transform: Transform::from_scale(Vec3::splat(6.0)),
             ..default()
         },
         animation_indices,
-        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        AnimationTimer(Timer::from_seconds(1.0, TimerMode::Repeating)),
     ));
 }
